f392586bf00e1d6348343718cdcf65a58ab10e29
Haruman Wiguna <harumanwiguna1@gmail.com>
2024-12-04 11:33:06 +0700

    Add Reset Button
    penambahan reset button

--- Assets/Scripts/MainMenuCanvasManager.cs
+++ Assets/Scripts/MainMenuCanvasManager.cs
@@ -4,17 +4,67 @@ using UnityEngine;
 
 public class MainMenuCanvasManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _mainMenuUI;
-    [SerializeField] private GameObject _stageSelectionUI;
-    [SerializeField] private GameObject _icAND;
-    [SerializeField] private GameObject _icOR;
-    [SerializeField] private GameObject _icNAND;
-    [SerializeField] private GameObject _icNOR;
-    [SerializeField] private GameObject _icNOT;
+    [SerializeField]
+    private GameObject _mainMenuUI;
+
+    [SerializeField]
+    private GameObject _stageSelectionUI;
+
+    [SerializeField]
+    private GameObject _icAND;
+
+    [SerializeField]
+    private GameObject _icOR;
+
+    [SerializeField]
+    private GameObject _icNAND;
+
+    [SerializeField]
+    private GameObject _icNOR;
+
+    [SerializeField]
+    private GameObject _icNOT;
+
+    [SerializeField]
+    private GameObject _ResetButton;
 
     private void Start()
     {
         OnOpenMainMenu();
+        CheckResetButtonVisibility();
+    }
+
+    public void CheckResetButtonVisibility()
+    {
+        if (
+            PlayerPrefs.HasKey("NandStage")
+            || PlayerPrefs.HasKey("AndStage")
+            || PlayerPrefs.HasKey("OrStage")
+            || PlayerPrefs.HasKey("NorStage")
+            || PlayerPrefs.HasKey("NotStage")
+            || PlayerPrefs.HasKey("XorStage")
+            || PlayerPrefs.HasKey("XnorStage")
+        )
+        {
+            _ResetButton.SetActive(true);
+        }
+        else
+        {
+            _ResetButton.SetActive(false);
+        }
+    }
+
+    public void OnResetButton()
+    {
+        PlayerPrefs.DeleteKey("NandStage");
+        PlayerPrefs.DeleteKey("NorStage");
+        PlayerPrefs.DeleteKey("NotStage");
+        PlayerPrefs.DeleteKey("OrStage");
+        PlayerPrefs.DeleteKey("AndStage");
+        PlayerPrefs.DeleteKey("XorStage");
+        PlayerPrefs.DeleteKey("XnorStage");
+        _ResetButton.SetActive(false);
+        Debug.Log("All stage data has been reset.");
     }
 
     public void OnOpenMainMenu()
@@ -22,7 +72,7 @@ public class MainMenuCanvasManager : MonoBehaviour
         _mainMenuUI.SetActive(true);
 
         _stageSelectionUI.SetActive(false);
-    }    
+    }
 
     public void OnOpenStageSelection()
     {
@@ -71,6 +121,7 @@ public class MainMenuCanvasManager : MonoBehaviour
         _icNOR.SetActive(false);
         _icNOT.SetActive(false);
     }
+
     public void OnOpenICNor()
     {
         _icNOR.SetActive(true);
